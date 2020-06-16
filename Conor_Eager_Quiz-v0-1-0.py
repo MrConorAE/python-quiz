a0f506a1bce4f094dbf9a3f827a4b8798a7a83a7
MrConorAE <26195325+MrConorAE@users.noreply.github.com>
2020-06-17 10:16:22 +1200

    Added the basic I/O handling functions (i, o, e and d)

--- Conor_Eager_Quiz-v0-1-0.py
+++ Conor_Eager_Quiz-v0-1-0.py
@@ -14,15 +14,51 @@ class Question:
         self.c = c
         self.d = d
         self.correct = r
-    title = "What is the answer to Life, the Universe and Everything?"
-    a = "420"
-    b = "69"
-    c = "0"
-    d = "42"
-    correct = "d"
 
 
-# FUNCTIONS ##
+## FUNCTIONS ##
+# Basic I/O with error handling/catching
+def i(t):
+    # Input function - accepts input from user and checks it is in correct format (t arg).
+    if t == "int":
+        while True:
+            try:
+                return int(input("<< "))
+            except:
+                o("Whoops, that's not a number! Please enter a whole number.")
+    elif t == "flt":
+        while True:
+            try:
+                return float(input("<< "))
+            except:
+                o("Whoops, that's not a float! Please enter a number.")
+    elif t == "str":
+        while True:
+            try:
+                return str(input("<< "))
+            except:
+                o("Whoops, that's not a string! Please enter some text.")
+    else:
+        e(f"invalid data type in i() function: {t}")
+
+
+def o(o):
+    # Output function - prints value passed to screen.
+    print(o)
+
+
+def e(o):
+    # Error output - prints an error to the screen with proper header.
+    print(f"[ERR]: {o}")
+
+
+def d(o):
+    # Debug output - prints debug information to the screen, but only if debug mode is enabled.
+    if (config.debug):
+        print(f"[DBG]: {o}")
+    else:
+        pass
+
 
 def importQuestions(f):
     try:
@@ -34,3 +70,5 @@ def importQuestions(f):
                     Question(split[0], split[1], split[2],
                              split[3], split[4], split[5])
                 )
+    except:
+        e("Could not read question file: invalid format.")
